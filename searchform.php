5cc2dae7b441523684f0bc286f0e0ecf676f6839
Stacey Knight-Davis <slknight@eiu.edu>
2020-08-04 15:33:21 -0500

    Create searchform.php

---
+++ searchform.php
@@ -0,0 +1,42 @@
+<form action="btccatsearchscript.php" method="get" target="_blank">
+	
+
+<fieldset>
+
+
+		Search for <input type="text" name="term" id="lookFor" size="25" value="" />
+             
+              
+              
+              
+           in <select name="filter" size="0" id="searchin">
+		      <option value="all">Entire Collection</option>
+		    
+		 
+		
+		      <option value="book">Books</option>
+		   
+		      <option value="kit">Kit</option>
+		
+		      
+		
+		      <option value="audio">Audio Books</option>
+	
+		
+		    
+		      </select> <input type="submit" name="submit" id="booksgo"  value="Go"/> <br />
+              
+              <div id="catradio"><label for="searchType" class="hidden">Search Type</label>
+               
+<label for="searchType2"><input type="radio" name="type" id="searchType2" value="any" checked />
+              Keywords</label>
+
+<label for="searchType0" ><input type="radio" name="type" id="searchType0" value="creator" />
+              Author  </label>
+              <label for="searchType1"><input type="radio"  name="type" id="searchType1" value="title" />
+              Title</label> </div>
+              
+              
+               
+		   </fieldset>
+ </form>
